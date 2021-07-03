acc83daa78a869e61e8afd0c49984fe4a32e5279
Andrew Webster <webstean@gmail.com>
2021-07-03 12:15:23 +1000

    qqq

--- etc/kamailio/install.sh
+++ etc/kamailio/install.sh
@@ -6,10 +6,6 @@ cp simple-sip-proxy/etc/kamailio/kamailio.cfg /etc/kamailio/
 cp simple-sip-proxy/etc/kamailio/tls.cfg /etc/kamailio/
 cp simple-sip-proxy/etc/kamailio/msteams.list /etc/kamailio/
 
+kamailio -v
 kamailio -c
 systemctl restart kamailio
-
-echo Build and Install Kamailio
-
-kamailio -v
-
