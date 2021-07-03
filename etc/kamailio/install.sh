5ce714bfb02fc586ceb176d02a6a37dcda23b5d0
Andrew Webster <webstean@gmail.com>
2021-07-03 12:05:50 +1000

    qqqq

--- etc/kamailio/install.sh
+++ etc/kamailio/install.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+rm -rf simple-sip-proxy
+git clone https://github.com/webstean/simple-sip-proxy
+cp simple-sip-proxy/etc/kamailio/kamailio.cfg /etc/kamailio/
+cp simple-sip-proxy/etc/kamailio/tls.cfg /etc/kamailio/
+cp simple-sip-proxy/etc/kamailio/msteams.list /etc/kamailio/
+
+kamailio -c
+systemctl restart kamailio
+
 echo Build and Install Kamailio
 
-sudo chown kamailio:kamailio /etc/kamailio/ca_list.pem
-sudo chmod 0644 /etc/kamailio/ca_list.pem
+kamailio -v
+
