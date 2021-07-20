b0a57e8d91f9e3e6f0cc60836745f6756768d2ff
Andrew Webster <webstean@gmail.com>
2021-07-21 01:08:47 +1000

    aaaaa

--- etc/kamailio/build.sh
+++ etc/kamailio/build.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
+if [ -d kamilio ] ; rm -rf kamilio ; fi
 git clone https://github.com/kamailio/kamailio kamailio
 cd kamailio
 git checkout -b 5.5 origin/5.5
 
-sudo apt-get install -y git-core
-sudo apt-get install -y gcc g++
-sudo apt-get install -y flex
-sudo apt-get install -y bison
-sudo apt-get install -y libmysqlclient-dev
-sudo apt-get install -y make
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libxml2-dev
-sudo apt-get install -y libpcre3-dev
-sudo apt-get install -y libjansson-dev
-sudo apt-get install -y libjson-c-dev
-sudo apt -y autoremove
+sudo apt-get install -y git-core gcc g++ flex bison libmysqlclient-dev make libssl-dev libcurl4-openssl-dev
+sudo apt-get install -y libxml2-dev libpcre3-dev libjansson-dev libjson-c-dev
+sudo apt-get install -y autoremove
 
 make include_modules="tls outbound jansson json" cfg
 make all
