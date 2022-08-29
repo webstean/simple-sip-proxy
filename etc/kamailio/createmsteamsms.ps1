867edb62f8d1976220d66bdbb9a423d0a817c29b
Andrew Webster <webstean@gmail.com>
2022-08-29 16:35:02 +1000

    Update createmsteamsms.ps1

--- etc/kamailio/createmsteamsms.ps1
+++ etc/kamailio/createmsteamsms.ps1
@@ -1,5 +1,18 @@
+
+$sp = New-AzADServicePrincipal -DisplayName ServicePrincipalName
+
+Connect-AzAccount -Identity
+ 
+
 $userCredential = Get-Credential
 $sfbSession = New-CsOnlineSession -Credential $userCredential
 Import-PSSession $sfbSession
 
 New-CsOnlinePSTNGateway -Fqdn SBC-DNS-DOMAIN -SipSignallingPort 5061 -MaxConcurrentSessions 10 -ForwardCallHistory $true -Enabled $true
+
+Set-CsOnlinePstnUsage -Identity Global -Usage @{Add="Unrestricted"}
+
+New-CsOnlineVoiceRoute -identity "Unrestricted" -NumberPattern ".*" -OnlinePstnGateway SBC-DNS-DOMAIN -Priority 1 -OnlinePstnUsages "Unrestricted"
+
+
+
