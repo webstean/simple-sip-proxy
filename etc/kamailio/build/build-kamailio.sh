1642d06f965de7100248793d476c451a875afa20
Andrew Webster <webstean@gmail.com>
2022-08-30 10:41:07 +1000

    Update build-kamailio.sh

--- etc/kamailio/build/build-kamailio.sh
+++ etc/kamailio/build/build-kamailio.sh
@@ -8,6 +8,7 @@ git checkout -b 5.5 origin/5.5
 sudo apt-get install -y git-core gcc g++ flex bison libmysqlclient-dev make libssl-dev libcurl4-openssl-dev
 sudo apt-get install -y libxml2-dev libpcre3-dev libjansson-dev libjson-c-dev
 sudo apt-get install -y autoremove
+
 # sqlite
 sudo apt-get install -y libsqlite3-0 libsqlite3-dev sqlite3
 
